b286f00276c11e5a0934998e3389ea19623b633a
KK <kaihghuang@simbo.com.tw>
2018-06-06 16:25:11 +0800

    add first main and loading

---
+++ src/Main.ts
@@ -0,0 +1,36 @@
+import { LoadingView } from "./LoadingView";
+
+export class Main {
+
+
+    private app: PIXI.Application;
+
+    constructor() {
+
+        this.initPIXI();
+        this.startLoading();
+
+    }
+
+    private initPIXI() {
+        this.app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+        document.body.appendChild(this.app.view);
+    }
+
+    startLoading(): any {
+        let loadingView = new LoadingView();
+        this.app.stage.addChild(loadingView);
+
+        PIXI.loader
+            .on("progress", (loader: PIXI.loaders.Loader, resource: PIXI.loaders.Resource) => { loadingView.updateProgress(loader.progress) })
+            .add("shops", "resource/shops.json")
+            .load(()=>{
+                this.onLoaded();
+            });
+
+    }
+
+    onLoaded(){
+        console.log("load done")
+    }
+}
